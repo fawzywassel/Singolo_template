898b53de0dfcc0d44357753439d7d445a8b84799
MacBook Pro <MacBookPro@MacBooks.local>
2016-10-17 12:11:11 +0300

    upadate 1

--- js/plugin.js
+++ js/plugin.js
@@ -1,7 +1,26 @@
-(function(){
+(function () {
     'use struct';
-    
-     new WOW().init();
+
+    new WOW().init();
     $('#our-work').mixItUp();
-    
+
+    $("ul.navbar-nav li").click(function () {
+        if (!$(this).hasClass("active")) {
+            $("ul.navbar-nav li.active").removeClass("active");
+            $(this).addClass("active");
+        }
+    });
+
+    $("ul.navbar-nav li a").click(function (e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        var scrollTo = $(target).offset().top;   
+        $('html,body').animate({scrollTop : scrollTo},1000); 
+    });
+
+
+
+
+
+
 }())
