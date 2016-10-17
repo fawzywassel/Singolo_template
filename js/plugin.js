2adff61438a61f8b8f64d067861f4abcffa79565
MacBook Pro <MacBookPro@MacBooks.local>
2016-10-17 17:22:48 +0300

    2nd update

--- js/plugin.js
+++ js/plugin.js
@@ -2,20 +2,22 @@
     'use struct';
 
     new WOW().init();
+    
     $('#our-work').mixItUp();
 
-    $("ul.navbar-nav li").click(function () {
-        if (!$(this).hasClass("active")) {
-            $("ul.navbar-nav li.active").removeClass("active");
-            $(this).addClass("active");
-        }
-    });
+//    $("ul.navbar-nav li").click(function () {
+//        if (!$(this).hasClass("active")) {
+//            $("ul.navbar-nav li.active").removeClass("active");
+//            $(this).addClass("active");
+//        }
+//    });
 
     $("ul.navbar-nav li a").click(function (e) {
         e.preventDefault();
         var target = $(this).attr('href');
-        var scrollTo = $(target).offset().top;   
-        $('html,body').animate({scrollTop : scrollTo},1000); 
+        var scrollTo = $(target).offset().top - 90;
+       
+        $('html,body').animate({scrollTop : scrollTo  },1000); 
     });
 
 
